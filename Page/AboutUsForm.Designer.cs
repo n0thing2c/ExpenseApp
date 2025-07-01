cd87ca63931667bf4eb2cdd3e70652e2d5a1b540
kinhquocne <kinhquocne@gmail.com>
2025-07-01 18:24:55 +0700

    01/07

--- Page/AboutUsForm.Designer.cs
+++ Page/AboutUsForm.Designer.cs
@@ -32,94 +32,139 @@ namespace Login.Page
         private void InitializeComponent()
         {
             this.labelTitle = new System.Windows.Forms.Label();
-            this.labelProject = new System.Windows.Forms.Label();
-            this.flowLayoutPanelMembers = new System.Windows.Forms.FlowLayoutPanel();
-            this.pictureBoxGroup = new System.Windows.Forms.PictureBox();
-            this.btnClose = new System.Windows.Forms.Button();
-            this.LLFacebook = new System.Windows.Forms.LinkLabel();
-            this.LLGitHub = new System.Windows.Forms.LinkLabel();
+            this.LBProject1 = new System.Windows.Forms.Label();
+            this.LBProject2 = new System.Windows.Forms.Label();
+            this.BodyPanel = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.labelDescription = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGroup)).BeginInit();
+            this.flowLayoutPanelMembers = new System.Windows.Forms.FlowLayoutPanel();
+            this.FooterPanel = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.LLGitHub = new System.Windows.Forms.LinkLabel();
+            this.LLFacebook = new System.Windows.Forms.LinkLabel();
+            this.BodyPanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.FooterPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // labelTitle
             // 
-            this.labelTitle.BackColor = System.Drawing.Color.Pink;
+            this.labelTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(120)))), ((int)(((byte)(144)))));
             this.labelTitle.Dock = System.Windows.Forms.DockStyle.Top;
-            this.labelTitle.Font = new System.Drawing.Font("Segoe UI", 18F);
-            this.labelTitle.ForeColor = System.Drawing.Color.Navy;
+            this.labelTitle.Font = new System.Drawing.Font("Gill Sans MT", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.ForeColor = System.Drawing.Color.Black;
             this.labelTitle.Location = new System.Drawing.Point(0, 0);
             this.labelTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelTitle.Name = "labelTitle";
-            this.labelTitle.Size = new System.Drawing.Size(1120, 70);
+            this.labelTitle.Size = new System.Drawing.Size(751, 61);
             this.labelTitle.TabIndex = 0;
-            this.labelTitle.Text = "labelTitle";
+            this.labelTitle.Text = "ABOUT US";
             this.labelTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // labelProject
+            // LBProject1
             // 
-            this.labelProject.Dock = System.Windows.Forms.DockStyle.Top;
-            this.labelProject.ForeColor = System.Drawing.Color.Navy;
-            this.labelProject.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.labelProject.Location = new System.Drawing.Point(0, 70);
-            this.labelProject.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.labelProject.Name = "labelProject";
-            this.labelProject.Size = new System.Drawing.Size(1120, 25);
-            this.labelProject.TabIndex = 1;
-            this.labelProject.Text = "labelProject";
-            this.labelProject.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LBProject1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.LBProject1.Font = new System.Drawing.Font("Gadugi", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBProject1.ForeColor = System.Drawing.Color.Black;
+            this.LBProject1.Location = new System.Drawing.Point(0, 0);
+            this.LBProject1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBProject1.Name = "LBProject1";
+            this.LBProject1.Size = new System.Drawing.Size(751, 50);
+            this.LBProject1.TabIndex = 1;
+            this.LBProject1.Text = "EXPENSE APP PROJECT";
+            this.LBProject1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // LBProject2
+            // 
+            this.LBProject2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.LBProject2.Font = new System.Drawing.Font("Gadugi", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBProject2.ForeColor = System.Drawing.Color.Black;
+            this.LBProject2.Location = new System.Drawing.Point(0, 50);
+            this.LBProject2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBProject2.Name = "LBProject2";
+            this.LBProject2.Size = new System.Drawing.Size(751, 33);
+            this.LBProject2.TabIndex = 8;
+            this.LBProject2.Text = "LAB PROJECT - OOP";
+            this.LBProject2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // BodyPanel
+            // 
+            this.BodyPanel.AutoScroll = true;
+            this.BodyPanel.Controls.Add(this.panel1);
+            this.BodyPanel.Controls.Add(this.LBProject2);
+            this.BodyPanel.Controls.Add(this.LBProject1);
+            this.BodyPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BodyPanel.Enabled = false;
+            this.BodyPanel.Location = new System.Drawing.Point(0, 61);
+            this.BodyPanel.Name = "BodyPanel";
+            this.BodyPanel.Size = new System.Drawing.Size(751, 456);
+            this.BodyPanel.TabIndex = 8;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelDescription);
+            this.panel1.Controls.Add(this.flowLayoutPanelMembers);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 83);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(751, 373);
+            this.panel1.TabIndex = 9;
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelDescription.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.labelDescription.Location = new System.Drawing.Point(0, 0);
+            this.labelDescription.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(751, 53);
+            this.labelDescription.TabIndex = 7;
+            this.labelDescription.Text = "Đây là ứng dụng quản lý...., hỗ trợ thêm, sửa, xoá";
+            this.labelDescription.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // flowLayoutPanelMembers
             // 
+            this.flowLayoutPanelMembers.AutoScroll = true;
+            this.flowLayoutPanelMembers.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.flowLayoutPanelMembers.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.flowLayoutPanelMembers.Location = new System.Drawing.Point(0, 208);
+            this.flowLayoutPanelMembers.Location = new System.Drawing.Point(0, 58);
             this.flowLayoutPanelMembers.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.flowLayoutPanelMembers.Name = "flowLayoutPanelMembers";
-            this.flowLayoutPanelMembers.Size = new System.Drawing.Size(255, 200);
+            this.flowLayoutPanelMembers.Size = new System.Drawing.Size(751, 315);
             this.flowLayoutPanelMembers.TabIndex = 2;
             this.flowLayoutPanelMembers.WrapContents = false;
             // 
-            // pictureBoxGroup
+            // FooterPanel
             // 
-            this.pictureBoxGroup.BackColor = System.Drawing.Color.Crimson;
-            this.pictureBoxGroup.Location = new System.Drawing.Point(0, 55);
-            this.pictureBoxGroup.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.pictureBoxGroup.Name = "pictureBoxGroup";
-            this.pictureBoxGroup.Size = new System.Drawing.Size(125, 78);
-            this.pictureBoxGroup.TabIndex = 3;
-            this.pictureBoxGroup.TabStop = false;
+            this.FooterPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(142)))), ((int)(((byte)(158)))));
+            this.FooterPanel.Controls.Add(this.btnClose);
+            this.FooterPanel.Controls.Add(this.LLGitHub);
+            this.FooterPanel.Controls.Add(this.LLFacebook);
+            this.FooterPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.FooterPanel.Location = new System.Drawing.Point(0, 517);
+            this.FooterPanel.Name = "FooterPanel";
+            this.FooterPanel.Size = new System.Drawing.Size(751, 65);
+            this.FooterPanel.TabIndex = 8;
             // 
             // btnClose
             // 
-            this.btnClose.BackColor = System.Drawing.Color.LightSlateGray;
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClose.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
-            this.btnClose.ForeColor = System.Drawing.Color.White;
-            this.btnClose.Location = new System.Drawing.Point(0, 468);
+            this.btnClose.ForeColor = System.Drawing.Color.Transparent;
+            this.btnClose.Location = new System.Drawing.Point(303, 10);
             this.btnClose.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(164, 41);
             this.btnClose.TabIndex = 4;
-            this.btnClose.Text = "button1";
+            this.btnClose.Text = "Exit";
             this.btnClose.UseVisualStyleBackColor = false;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
-            // 
-            // LLFacebook
-            // 
-            this.LLFacebook.AutoSize = true;
-            this.LLFacebook.Location = new System.Drawing.Point(-1, 413);
-            this.LLFacebook.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.LLFacebook.Name = "LLFacebook";
-            this.LLFacebook.Size = new System.Drawing.Size(91, 25);
-            this.LLFacebook.TabIndex = 5;
-            this.LLFacebook.TabStop = true;
-            this.LLFacebook.Text = "Facebook";
-            this.LLFacebook.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LLFacebook_LinkClicked);
             // 
             // LLGitHub
             // 
             this.LLGitHub.AutoSize = true;
-            this.LLGitHub.Location = new System.Drawing.Point(2, 438);
+            this.LLGitHub.Location = new System.Drawing.Point(112, 26);
             this.LLGitHub.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.LLGitHub.Name = "LLGitHub";
             this.LLGitHub.Size = new System.Drawing.Size(71, 25);
@@ -128,54 +173,54 @@ namespace Login.Page
             this.LLGitHub.Text = "GitHub";
             this.LLGitHub.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LLGitHub_LinkClicked);
             // 
-            // labelDescription
+            // LLFacebook
             // 
-            this.labelDescription.AutoSize = true;
-            this.labelDescription.Font = new System.Drawing.Font("Segoe UI", 10F);
-            this.labelDescription.Location = new System.Drawing.Point(0, 162);
-            this.labelDescription.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.labelDescription.MaximumSize = new System.Drawing.Size(600, 0);
-            this.labelDescription.Name = "labelDescription";
-            this.labelDescription.Size = new System.Drawing.Size(132, 23);
-            this.labelDescription.TabIndex = 7;
-            this.labelDescription.Text = "labelDescription";
-            this.labelDescription.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LLFacebook.AutoSize = true;
+            this.LLFacebook.Location = new System.Drawing.Point(13, 26);
+            this.LLFacebook.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LLFacebook.Name = "LLFacebook";
+            this.LLFacebook.Size = new System.Drawing.Size(91, 25);
+            this.LLFacebook.TabIndex = 5;
+            this.LLFacebook.TabStop = true;
+            this.LLFacebook.Text = "Facebook";
+            this.LLFacebook.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LLFacebook_LinkClicked);
             // 
             // AboutUsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(1120, 703);
-            this.Controls.Add(this.LLGitHub);
-            this.Controls.Add(this.LLFacebook);
-            this.Controls.Add(this.labelDescription);
-            this.Controls.Add(this.pictureBoxGroup);
-            this.Controls.Add(this.btnClose);
-            this.Controls.Add(this.flowLayoutPanelMembers);
-            this.Controls.Add(this.labelProject);
+            this.ClientSize = new System.Drawing.Size(751, 582);
+            this.Controls.Add(this.BodyPanel);
+            this.Controls.Add(this.FooterPanel);
             this.Controls.Add(this.labelTitle);
             this.Font = new System.Drawing.Font("Segoe UI", 11F);
-            this.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.ForeColor = System.Drawing.Color.Black;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "AboutUsForm";
             this.Text = "AboutUsForm";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGroup)).EndInit();
+            this.BodyPanel.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.FooterPanel.ResumeLayout(false);
+            this.FooterPanel.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Label labelTitle;
-        private System.Windows.Forms.Label labelProject;
+        private System.Windows.Forms.Label LBProject1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelMembers;
-        private System.Windows.Forms.PictureBox pictureBoxGroup;
         private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.LinkLabel LLFacebook;
         private System.Windows.Forms.LinkLabel LLGitHub;
         private System.Windows.Forms.Label labelDescription;
+        private Panel BodyPanel;
+        private Panel FooterPanel;
+        private Label LBProject2;
+        private Panel panel1;
     }
 }
--- Page/AboutUsForm.cs
+++ Page/AboutUsForm.cs
@@ -25,40 +25,16 @@ namespace Login.Page
 
             LoadTeamInfo();
             this.MouseDown += Form_MouseDown;
-            CenterLabel(labelTitle);
-            CenterLabel(labelDescription);
-            CenterFlowLayoutPanel();
             CenterExitButton();
         }
         private void LoadTeamInfo()
         {
-            labelTitle.Text = "ABOUT US";
-            labelProject.Text = "Dự án: ...";
-            pictureBoxGroup.Size = new Size(100, 100); // hoặc theo ảnh bạn muốn
-            pictureBoxGroup.SizeMode = PictureBoxSizeMode.Zoom; // hiển thị ảnh đẹp
-            pictureBoxGroup.Left = (this.ClientSize.Width - pictureBoxGroup.Width) / 2;
-            pictureBoxGroup.Top = labelProject.Bottom + 10;
-            labelDescription.Text = "Đây là ứng dụng quản lý...., hỗ trợ thêm, sửa, xoá " +
-                        "và tìm kiếm thông tin .....";
-            labelDescription.MaximumSize = new Size((int)(this.ClientSize.Width * 0.8), 0);
-            labelDescription.AutoSize = true;
-            labelDescription.TextAlign = ContentAlignment.MiddleCenter;
-            labelDescription.Left = (this.ClientSize.Width - labelDescription.Width) / 2;
-            labelDescription.Top = pictureBoxGroup.Bottom + 10; 
-            flowLayoutPanelMembers.Top = labelDescription.Bottom + 20; 
-
-
-
-            btnClose.Text = "Exit";
             // Thêm thông tin từng thành viên
-            AddMember("Nguyễn Kinh Quốc", "...", "Phụ trách thiết kế About us.");
-            AddMember("Trần Công Hoàng Tấn", "...", "....");
-            AddMember("Trần Nguyễn Duy Thịnh", "...", "....");
-            AddMember("Huỳnh Gia Khang", "...", "....");
-            AddMember("Trương Tấn Dũng", "...", "....");
-            labelDescription.MaximumSize = new Size((int)(this.ClientSize.Width * 0.8), 0);
-            labelDescription.AutoSize = true;
-            CenterSocialLinks();
+            AddMember("Huỳnh Gia Khang", "Developer, report writer", "Design classes for file handling\r\nDesign UI and logics for Management windows\r\n\r\n");
+            AddMember("Trần Nguyễn Duy Thịnh", "Developer, report writer", "Design classes for user account handling\r\nDesign UI and authentication for Login/Sign up windows\r\n");
+            AddMember("Trương Tấn Dũng", "Interface and System Designer, report writer", "Design system structure and GUI for Menu");
+            AddMember("Trần Công Hoàng Tấn", "Developer, report writer", "Design UI and logics for Reports and Profile windows");
+            AddMember("Nguyễn Kinh Quốc", "Developer, report writer", "Design UI and logics for About us window");
             ToolTip tip = new ToolTip();
             tip.SetToolTip(LLFacebook, "Facebook của nhóm");
             tip.SetToolTip(LLGitHub, "GitHub của nhóm");
@@ -71,6 +47,7 @@ namespace Login.Page
             lbl.AutoSize = true;
             lbl.Margin = new Padding(10);
             lbl.Font = new Font("Segoe UI", 9);
+            lbl.ForeColor = Color.Black;
             flowLayoutPanelMembers.Controls.Add(lbl); // flowLayoutPanelMembers đã có sẵn trong Form
         }
 
@@ -78,32 +55,11 @@ namespace Login.Page
         {
             this.Close();
         }
-        private void CenterLabel(Label lbl)
-        {
-            lbl.Left = (this.ClientSize.Width - lbl.Width) / 2;
-        }
 
         private void CenterExitButton()
         {
             // Căn giữa theo chiều ngang
             btnClose.Left = (this.ClientSize.Width - btnClose.Width) / 2;
-
-            // Đặt ở dưới cùng cách đáy 20px
-            btnClose.Top = this.ClientSize.Height - btnClose.Height - 20;
-        }
-
-        private void CenterFlowLayoutPanel()
-        {
-            int targetWidth = (int)(this.ClientSize.Width * 0.8);
-            flowLayoutPanelMembers.Width = targetWidth;
-
-            // Đặt chiều cao hợp lý
-            flowLayoutPanelMembers.Height = 180; // hoặc tùy chỉnh
-
-            flowLayoutPanelMembers.Left = (this.ClientSize.Width - targetWidth) / 2;
-
-            // Căn phía dưới labelDescription
-            flowLayoutPanelMembers.Top = labelDescription.Bottom + 20; // cách 20px
         }
 
         private void LLFacebook_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
@@ -125,26 +81,7 @@ namespace Login.Page
         }
         private void AboutUsForm_Resize(object sender, EventArgs e)
         {
-            CenterExitButton();
-            CenterFlowLayoutPanel();
-            CenterLabel(labelTitle);
-            CenterLabel(labelDescription);
-            CenterSocialLinks();
-        }
-        private void CenterSocialLinks()
-        {
-            int spacing = 20;
-            int totalWidth = LLFacebook.Width + spacing + LLGitHub.Width;
 
-            int startX = (this.ClientSize.Width - totalWidth) / 2;
-
-            LLFacebook.Left = startX;
-            LLGitHub.Left = LLFacebook.Right + spacing;
-
-            // Nằm sát đáy: đặt TOP phía trên nút Exit 5px
-            int linkTop = this.ClientSize.Height - btnClose.Height - LLFacebook.Height - 20;
-
-            LLFacebook.Top = LLGitHub.Top = linkTop;
         }
 
 
